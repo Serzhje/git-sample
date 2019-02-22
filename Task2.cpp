e5c00c71da098b6214395d9690215ce4166db4c1
Serzhje1 <7prite@gmail.com>
2019-02-22 22:12:40 +0300

    First commit

---
+++ Task2.cpp
@@ -0,0 +1,23 @@
+//#include <cstdlib>
+#include <iostream>
+#include <locale>
+
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, ".1251");
+	cout << "Введите символьную строку" << endl;
+	
+	//Массиив для символьной строки
+	char Word[100];
+	// Вводим слово с консоли
+	cin >> Word;
+
+	// Выводим слово
+	cout << "Это слово " << Word << endl;
+
+	for (int i = 0; Word[i] != 0; i++)
+
+	cin.get();
+}
